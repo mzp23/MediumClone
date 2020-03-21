9b39182fd74e2cbc1a6e645b973063282941aa5d
mzp23 <mzgme23@gmail.com>
2020-03-21 23:34:26 +0200

    add routes

---
+++ src/pages/article/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Article = () => {
+  return <div> Article Fedd іфвіфвіфв </div>;
+};
+
+export default Article;
---
+++ src/pages/globalFeed/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GlobalFeed = () => {
+  return <div> GLobal Fedd іфвіфвіфв </div>;
+};
+
+export default GlobalFeed;
