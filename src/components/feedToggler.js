5ec6a3934365aa6e6c1e20a9f278e5d02effacdf
mzp23 <mzgme23@gmail.com>
2020-03-24 12:42:41 +0200

    implement your feed functionality

--- src/components/feedToggler.js
+++ src/components/feedToggler.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { CurrentUserContext } from '../contexts/currentUser';
+
 const FeedToggler = ({ tagName }) => {
+    const [currentUserState] = useContext(CurrentUserContext);
+
     return (
         <div className='feed-toggle'>
             <ul className='nav nav-pills outline-active'>
-                <li className='nav-item'>
-                    <NavLink to='/feed' className='nav-link'>
-                        Your Feed
+                {currentUserState.isLoggedIn && (
+                    <li className='nav-item'>
+                        <NavLink to='/feed' className='nav-link'>
+                            Your Feed
                     </NavLink>
-                </li>
+                    </li>
+                )}
                 <li className='nav-item'>
                     <NavLink to='/' className='nav-link' exact>
                         Glabal Feed
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,14 @@ import GlobalFeed from './pages/globalFeed';
 import Article from './pages/article';
 import Authentication from './pages/auth';
 import TagFeed from './pages/tagFeed'
+import YourFeed from './pages/yourFeed'
+
 
 const Routes = () => {
   return (
     <Switch>
       <Route path='/' component={GlobalFeed} exact />
+      <Route path='/feed' component={YourFeed} exact />
       <Route path='/tags/:slug' component={TagFeed} />
       <Route path='/articles/:slug' component={Article} />
       <Route path='/login' component={Authentication} />
