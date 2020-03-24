07b498fa63e8108ed49f14114560769cbb91835a
mzp23 <mzgme23@gmail.com>
2020-03-24 23:42:17 +0200

    implement edit article functionality

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 import GlobalFeed from './pages/globalFeed';
 import Article from './pages/article';
 import Authentication from './pages/auth';
-import TagFeed from './pages/tagFeed'
-import YourFeed from './pages/yourFeed'
-import CreateArticle from './pages/createArticle/'
+import TagFeed from './pages/tagFeed';
+import YourFeed from './pages/yourFeed';
+import CreateArticle from './pages/createArticle/';
+import EditArticle from './pages/editArticle/';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path='/' component={GlobalFeed} exact />
-      <Route path='/articles/new' component={CreateArticle} exact />
+      <Route path='/articles/new' component={CreateArticle} />
+      <Route path='/articles/:slug/edit' component={EditArticle} exact />
       <Route path='/feed' component={YourFeed} exact />
       <Route path='/tags/:slug' component={TagFeed} />
       <Route path='/articles/:slug' component={Article} />
